82e58624627ae24e782b2c0c796ee4a804f4c679
Kevin_Gillispie <oakenquill@gmail.com>
2017-08-23 14:07:47 -0700

    init commit

---
+++ ChallengeEpicSpiesAssignment/NewAssignmentForm.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewAssignmentForm.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.NewAssignmentForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>New Assignment Form</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 242px;
+            height: 300px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="Epic Spy Logo" class="auto-style1" src="epic-spies-logo.jpg" style="height: 190px; width: auto;" /><br />
+        </div>
+        <div><h3 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Spy New Assignment Form</h3>
+            <p style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">&nbsp;</p></div>
+        <div>Spy Code Name: <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+        <div>New Assignment Name: <asp:TextBox ID="newAssignTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+        <div><p>End of Previous Assignment:
+            <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar></p>
+            <br />
+        </div>
+        <div><p>Start of New Assignment:
+            <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar></p>
+            <br />
+        </div>
+        <div><p>Prjected End Date of New Assignment:
+            <asp:Calendar ID="Calendar3" runat="server"></asp:Calendar></p>
+            <p>&nbsp;</p>
+        </div>
+        <div>
+            <asp:Button ID="assignButton" runat="server" Text="Assign Spy" OnClick="assignButton_Click" />
+            <br />
+            <br />
+        </div>
+        <div>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label></div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/NewAssignmentForm.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class NewAssignmentForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Calendar1.SelectedDate = DateTime.Today;
+            Calendar2.SelectedDate = DateTime.Today.AddDays(14);
+            // Calendar3.SelectedDate = DateTime.Today.AddDays(21);
+
+            Calendar2.VisibleDate = Calendar2.SelectedDate;
+            // Calendar3.VisibleDate = Calendar3.SelectedDate;
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+
+            double vacationDays = Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate).TotalDays;
+            double jobLengthDays = Calendar3.SelectedDate.Subtract(Calendar2.SelectedDate).TotalDays;
+
+            if (vacationDays < 14)
+            {
+                resultLabel.Text = "ERROR: New assignments must begin no sooner than two weeks after the end of previous assignment.";
+            }
+            else if (vacationDays >= 14 && jobLengthDays > 21)
+            {
+                double cost = (jobLengthDays * 500) + 1000;
+                string result = String.Format(codeNameTextBox.Text + " is assigned to " + newAssignTextBox.Text + "."
+                    + "<br>" + "Budget total: {0:C}", cost);
+
+                resultLabel.Text = result;
+            }
+            else
+            {
+                double cost = (jobLengthDays * 500);
+                string result = String.Format(codeNameTextBox.Text + " is assigned to " + newAssignTextBox.Text + "."
+                    + "<br>" + "Budget total: {0:C}", cost);
+
+                resultLabel.Text = result;
+            }
+        }
+    }
+}
